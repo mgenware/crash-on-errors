56694252474c2950607b4ec1c35e901487b04f0e
Mgen <m@mgenware.com>
2020-04-26 00:45:48 +0800

    Remove unnecessary spaces in logs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 
 process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception: ', err.message);
+  console.error('Uncaught Exception:', err.message);
   process.exit(1);
 });
 
 process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection at: ', promise, 'reason: ', reason);
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
   process.exit(1);
 });
